18af132648fb16664f1f71a74d815e2dbf8e1a3e
cnzcy <jszcy@qq.com>
2019-08-13 22:39:07 +0800

    5.1-5.2 UDP搜索

---
+++ demo/src/main/java/io/github/demo/client/Client.java
@@ -0,0 +1,8 @@
+package io.github.demo.client;
+
+public class Client {
+    public static void main(String[] args) {
+        ServerInfo info = ClientSearcher.searchServer(10000);
+        System.out.println("Server:" + info);
+    }
+}
---
+++ demo/src/main/java/io/github/demo/server/Server.java
@@ -0,0 +1,20 @@
+package io.github.demo.server;
+
+import java.io.IOException;
+
+import io.github.demo.constants.TCPConstants;
+
+public class Server {
+    public static void main(String[] args) {
+        ServerProvider.start(TCPConstants.PORT_SERVER);
+
+        try {
+            //noinspection ResultOfMethodCallIgnored
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ServerProvider.stop();
+    }
+}
