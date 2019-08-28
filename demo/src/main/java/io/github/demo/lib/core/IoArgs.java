5db373077d9341b1c072df5980529c32089f7d64
cnzcy <jszcy@qq.com>
2019-08-28 23:31:43 +0800

    8.2_复现消息不全

--- demo/src/main/java/io/github/demo/lib/core/IoArgs.java
+++ demo/src/main/java/io/github/demo/lib/core/IoArgs.java
@@ -8,6 +8,16 @@ import java.nio.channels.SocketChannel;
  * 封装ByteBuffer
  */
 public class IoArgs {
+    // 复现1：消息不全
+    // 发送超过4个byte的长度就复现了
+    // 4864728b-e623-4992-a3d3-8df68e7d7255:123
+    // 长度：3
+    // 4864728b-e623-4992-a3d3-8df68e7d7255:567
+    // 长度：3
+    // 4864728b-e623-4992-a3d3-8df68e7d7255:90
+    // 长度：3
+    // 而实际发送的是1234567890
+    // private byte[] byteBuffer = new byte[4];
     private byte[] byteBuffer = new byte[256];
     private ByteBuffer buffer = ByteBuffer.wrap(byteBuffer);
 
